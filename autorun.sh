5825a8d3a453fc1239e9d1ee47547a37ef474172
nicolas-husson <nicolas-husson@debian>
2016-06-01 15:37:00 +0200

    move needed package to install && correct wrong quote

--- autorun.sh
+++ autorun.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#Require sudo && apt-transport-https to be install and running
+#Require sudo && curl to be install and running
 #as root run 'apt install sudo && adduser <username> sudo'
 #remember to relogin when you do that 
 
 echo 'Installing build-essential git terminator...'
-sudo apt -y install build-essential git terminator curl wget vim
+sudo apt -y install build-essential git terminator curl wget vim apt-transport-https
 
 echo 'Generate ssh key for ssh'
 eval "$(ssh-agent -s)"
@@ -18,7 +18,7 @@ sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stabl
 sudo apt-get update
 sudo apt-get -y install google-chrome-stable
 
-echo 'Install of openjdk 7"
+echo 'Install of openjdk 7'
 sudo apt -y install openjdk-7-jdk
 
 echo 'Install zsh && oh-myzsh'
