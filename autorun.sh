bbd37490a3dc355e990276712e7da9ba0e3c4635
nicolas-husson <nicolas-husson@debian>
2016-06-01 13:40:07 +0200

    adding installation of ohmyzsh && curl && wget

--- autorun.sh
+++ autorun.sh
@@ -5,7 +5,7 @@
 #remember to relogin when you do that 
 
 echo 'Installing build-essential git terminator...'
-sudo apt -y install build-essential git terminator
+sudo apt -y install build-essential git terminator curl wget 
 
 echo 'Generate ssh key for ssh'
 eval "$(ssh-agent -s)"
@@ -17,3 +17,7 @@ echo 'Install google chrome stable'
 sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
 sudo apt-get -y install google-chrome-stable
+
+echo 'Install zsh && oh-myzsh'
+sudo apt -y install zsh 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
