cbd6d607896d6172b2bb516a43470fadafb758ef
nicolas-husson <nicolas-husson@debian>
2016-06-02 11:53:52 +0200

    autoaccept install of java 8 oracle

--- autorun.sh
+++ autorun.sh
@@ -29,7 +29,7 @@ function succeed {
     sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
     sudo apt-get update
-    sudo apt-get install oracle-java8-installer
+    sudo apt-get -y install oracle-java8-installer
 
     echo 'Install zsh && oh-myzsh'
     sudo apt -y install zsh
